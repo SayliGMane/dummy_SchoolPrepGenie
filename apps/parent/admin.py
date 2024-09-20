b415851eb6cb13dde69e82be77bfe77db7475ed6
Ginu <ginujestin@gmail.com>
2024-09-20 09:09:18 +0200

    added tabular inline

--- apps/parent/admin.py
+++ apps/parent/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models.parent import Parent
+from apps.student.models.student import Student
+
+
+class StudentInline(admin.TabularInline):  
+    model = Student
+    extra = 1 
 
 class ParentAdmin(admin.ModelAdmin):
     
-    list_display = ('id','full_name','username','gender','address','phone_number','email','date_of_join')
+    list_display = ('id','full_name','username','gender','address','phone_number','email','date_of_join','student_names')
+    
+    inlines = [StudentInline] 
 
     def full_name(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
@@ -17,6 +25,12 @@ class ParentAdmin(admin.ModelAdmin):
     def username(self, obj):
         return obj.user.username
     username.short_description = 'Username'
+    
+    def student_names(self, obj):
+        
+        students = obj.children.all()
+        return ", ".join([f"{student.first_name} {student.last_name}" for student in students]) if students else "No students"
+    student_names.short_description = 'Students'
 
 
 admin.site.register(Parent,ParentAdmin)
